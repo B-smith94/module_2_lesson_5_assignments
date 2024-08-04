760d61801988f042d20337ec340925cbedb604a9
b-smith94 <byrondsmith9@gmail.com>
2024-08-04 12:49:10 -0400

    Got subtraction to work properly

--- calculator_app.py
+++ calculator_app.py
@@ -14,21 +14,21 @@ def calculator():
             continue
         else:
             break
-    num1 = float(input("Enter the first number:\t"))
-    num2= float(input("enter the second number:\t"))
+    num1 = float(input("Enter the first number:\n"))
+    num2 = float(input("enter the second number:\n"))
     num_list = [num1, num2]
     for answer in num_list:
         if task == "add":
             print(f"Answer: {sum(num_list)}")
             break
-        elif task == "subract":
-            print(f"Answer: {num_list[0] - num_list[1]}")
-            break
+        
         elif task == "multiply":
             print(f"Answer: {num_list[0] * num_list[1]}")
             break
         elif task == "divide":
             print(f"Answer: {num_list[0] / num_list[1]}")
             break
-
+        else:
+            print(f"Answer: {num_list[0] - num_list[1]}")
+            break
 calculator()
