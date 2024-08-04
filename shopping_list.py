e3e408c1dbdcdd56d527a8e43284973a195519e3
b-smith94 <byrondsmith9@gmail.com>
2024-08-04 14:34:16 -0400

    added inputs asking if user would like to make changes, add or remove from the list, and add a separator

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,27 @@
 #1. Write a function that lets the user add items to the list.
-#2. Include a function that prints out the entire list in a formatted way.
+#2. Include a function to remove items from the list.
+#3. Add a function that prints out the entire list in a formatted way.
+#NOTE: The goal of this is to be a program.  Reccomend using a while loop to allow user to continue to add, remove,
+# and print off their shopping list until they decide to "quit" (break the loop).
 
+def shopping_list(items=[]):
+    while True:
+        changes = input("Would you like to make changes to your list? (yes/no)\t")
+        while changes == "yes":
+            add_or_remove = input("Ok, would you like to add to the list or take items away? (type 'add' or 'remove')\n")
+            if add_or_remove == "add":
+                add_items = items.append(input("what would you like to add?\t"))
+                break
+            elif add_or_remove == "remove":
+                remove_items = items.remove(input("ok, what would you like to remove?\t"))
+                break
+            else:
+                print("Invalid input, please try again.")
+                continue
+        else:
+            separator = input("How would you like your list formatted? Please type '-', '--', or ', '.\t")
+            new_list = separator.join(items)
+            break
+    print(new_list)
+
+shopping_list(['tomatoes', 'peppers', 'cereal'])
