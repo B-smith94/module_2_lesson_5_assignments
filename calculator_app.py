a299e25a922dfd47181a5931d682abd3e6e0cd35
b-smith94 <byrondsmith9@gmail.com>
2024-08-04 13:34:02 -0400

    added error handling to calculator app

--- calculator_app.py
+++ calculator_app.py
@@ -14,21 +14,37 @@ def calculator():
             continue
         else:
             break
-    num1 = float(input("Enter the first number:\n"))
-    num2 = float(input("enter the second number:\n"))
+    while True:
+        try:
+            num1 = float(input("Enter the first number:\n"))
+        except ValueError:
+            print("Invalid input, please try again.")
+            continue
+        break
+    while True:
+        try:
+            num2 = float(input("enter the second number:\n"))
+        except ValueError:
+            print("Invalid input, please try again.")
+            continue
+        break
     num_list = [num1, num2]
     for answer in num_list:
         if task == "add":
             print(f"Answer: {sum(num_list)}")
             break
-        
         elif task == "multiply":
             print(f"Answer: {num_list[0] * num_list[1]}")
             break
         elif task == "divide":
-            print(f"Answer: {num_list[0] / num_list[1]}")
+            try:
+                result = num_list[0] / num_list[1]
+            except ZeroDivisionError:
+                result = 0
+            print(f"Answer: {result}")
             break
         else:
             print(f"Answer: {num_list[0] - num_list[1]}")
             break
+
 calculator()
